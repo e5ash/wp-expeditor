cb0f474704e9141d2f534fead7ee8d1c9fc61b8b
e5ash <io@e5ash.com>
2018-01-04 12:42:34 +0300

    Upload default files

---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package name
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php if ( is_front_page() && is_home() ) : ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"></a><?php endif; ?>
+
+<?php 
+	wp_nav_menu( array(
+		'theme_location' => 'menu-main'
+	) );
+?>
+
+<ul class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <?php if(function_exists('bcn_display'))
+    {
+        bcn_display();
+    }?>
+</ul>
